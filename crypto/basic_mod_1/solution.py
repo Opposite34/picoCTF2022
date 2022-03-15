01eaf903f31eb856e4236d67a7f3ece7adb4f4d3
BrianLamWC <BrianLamWC@gmail.com>
2022-03-16 05:11:22 +0900

    readme for basic1

--- crypto/basic_mod_1/solution.py
+++ crypto/basic_mod_1/solution.py
@@ -2,7 +2,7 @@ arr = [128,63,131,198,262,110,309,73,276,285,316,161,151,73,219,150,145,217,103,
 moddedArr = []
 
 for num in arr:
-    moddedArr.append(num % 37)
+    moddedArr.append(num % 41)
 
 print(moddedArr)
 
