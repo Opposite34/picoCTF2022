d9c9a57da8637dcc2b5c76921956d3dd1b695412
BrianLamWC <BrianLamWC@gmail.com>
2022-03-16 23:16:29 +0900

    basic2

--- crypto/basic_mod_1/solution.py
+++ crypto/basic_mod_1/solution.py
@@ -2,18 +2,18 @@ arr = [128,63,131,198,262,110,309,73,276,285,316,161,151,73,219,150,145,217,103,
 moddedArr = []
 
 for num in arr:
-    moddedArr.append(num % 41)
+    moddedArr.append(pow(num, -1, 41))
 
 print(moddedArr)
 
 decoded = ""
 
 for num in moddedArr:
-    if num < 26:
-        num += 65
+    if num < 27:
+        num += 64
 
-    elif num < 36:
-        num += 22
+    elif num < 37:
+        num += 21
 
     else:
         num = 95
--- crypto/basic_mod_2/solution.py
+++ crypto/basic_mod_2/solution.py
@@ -1,1 +1,23 @@
 arr = [350,372,192,354,139,337,67,311,392,338,241,414,180,277,379,294,128,117,250,404,336,350,386]
+moddedArr = []
+
+for num in arr:
+    moddedArr.append(pow(num, -1, 41))
+
+print(moddedArr)
+
+decoded = ""
+
+for num in moddedArr:
+    if num < 27:
+        num += 64
+
+    elif num < 37:
+        num += 21
+
+    else:
+        num = 95
+
+    decoded += chr(num)
+
+print(decoded)
